ad155e30c4f111e6bff2c563bb93bcccf88efbd3
German <w1ss@yandex.ru>
2023-10-11 13:38:06 +0300

    vintage+Junit5

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -23,4 +23,23 @@ public class CashbackHackServiceTest {
 
         org.junit.Assert.assertEquals(expected, actual);
     }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainJunit5() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    public void testRemainWithCashbackJunit5() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
 }
